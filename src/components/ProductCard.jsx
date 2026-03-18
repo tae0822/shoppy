b3262936ef3117904c773d8f1cf74df363903314
MisoWan <jopd4@naver.com>
2026-03-17 20:14:10 -0400

    Change Image Hiehgt in ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -14,7 +14,7 @@ export default function ProductCard({
       }}
       className='rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'
     >
-      <img className='w-full' src={image} alt={title} />
+      <img className='w-full max-h-[600px]' src={image} alt={title} />
       <div className='mt-2 px-2 text-lg flex justify-between items-center'>
         <h3 className='truncate'>{title}</h3>
         <p>{`$${price}`}</p>
